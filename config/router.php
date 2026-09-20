9d6c6f7635d444fb6a3e3a6f7b2b0953dd3d43b0
Abdellah DAKIR ALLAH <dal.dakirallah@gmail.com>
2026-09-20 20:58:09 +0100

    google oauth updates

--- config/router.php
+++ config/router.php
@@ -1,2 +1,7 @@
 <?php
-define("BASE_URL", "http://localhost/my-notes");
+require "autoload.php";
+
+$dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
+$dotenv->load();
+
+define("BASE_URL", $_ENV["BASE_URL"]);
