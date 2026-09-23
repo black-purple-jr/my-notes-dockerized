25bc9954a8014c434fed930efa3a606496e8e06a
Abdellah DAKIR ALLAH <dal.dakirallah@gmail.com>
2026-09-23 20:36:04 +0000

    name conflict fix

--- config/session.php
+++ config/session.php
@@ -6,7 +6,7 @@ ini_set("session.use_strict_mode", 1);
 
 session_set_cookie_params([
   "lifetime" => 1800,
-  "domain" => "localhost",
+  "domain" => "my-notes.bpxdevjr.me",
   "path" => "/",
   "secure" => isset($_SERVER['HTTPS']) && $_SERVER['HTTPS'] !== 'off',
   "httponly" => true
