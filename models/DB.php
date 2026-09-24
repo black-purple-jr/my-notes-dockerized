5f173bb84f29d9c58f1fa4c7420862bf99448101
Abdellah DAKIR ALLAH <dal.dakirallah@gmail.com>
2026-09-24 12:46:24 +0000

    test

--- models/DB.php
+++ models/DB.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require __DIR__ . "/../vendor/autoload.php";
+require_once __DIR__ . "/../vendor/autoload.php";
 
 $dotenv = Dotenv\Dotenv::createImmutable(__DIR__ . "/..");
 $dotenv->safeLoad();
--- models/User.php
+++ models/User.php
@@ -2,7 +2,7 @@
 
 declare(strict_types=1);
 
-require_once "DB.php";
+require __DIR__ . "DB.php";
 
 class User extends DB
 {
